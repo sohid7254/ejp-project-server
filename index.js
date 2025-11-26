7eeae53883886197b74ba0d80fcbf23c1e2b1c4e
sohid7254 <sohidameen321@gmail.com>
2025-11-27 01:02:48 +0600

    Deploying Projects

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const db = client.db("ejp-project");
         const productsCollection = db.collection("products");
@@ -68,7 +68,7 @@ async function run() {
             res.json(result);
         });
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
