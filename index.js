4049317dcbba67be483c540f9197371d4abdf751
sohid7254 <sohidameen321@gmail.com>
2025-11-26 15:48:01 +0600

    Added Products server where anyone can see all the products that i have on frontend

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 4000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// midlewear
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fdcjmvl.mongodb.net/?appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const db = client.db("ejp-project");
+        const productsCollection = db.collection("products");
+
+        // get products
+
+        app.get("/products", async (req, res) => {
+            const result = await productsCollection.find().limit(6).toArray();
+            res.send(result);
+        });
+
+        // ✅ Get all products (with optional search by title)
+        app.get("/allproducts", async (req, res) => {
+            const search = req.query.search || ""; 
+            let query = {};
+
+            if (search) {
+                query = { title: { $regex: search, $options: "i" } };
+                
+            }
+
+            const result = await productsCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("EJP is running here");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
